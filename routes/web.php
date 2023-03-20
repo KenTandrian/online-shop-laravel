6a996872fa30181ebf7f0013ecf1f67b9d4f9a42
KenTandrian <kenricktan11@gmail.com>
2023-03-20 11:20:12 +0700

    feat: cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,24 +14,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/halo/{id}', function ($id) {
-    return "Hello World {$id}";
-});
-
-// Route untuk akses view latihan1.php
-Route::get('/latihan1/{nama}', function($nama) {
-    return view('latihan1', ['nama' => $nama]);
-});
-
-// Route untuk akses view latihan2.php
-Route::get('/latihan2/{nama}', function($nama) {
-    return view('latihan2', ['nama' => $nama]);
-});
-
-Route::resource('siswa', 'SiswaController');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
