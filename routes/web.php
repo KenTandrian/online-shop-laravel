4218cedf4768661fa4447de2d8008def6dd0f819
KenTandrian <kenricktan11@gmail.com>
2023-03-20 12:00:32 +0700

    feat: add list route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/list', function () {
+    return view('list');
+});
