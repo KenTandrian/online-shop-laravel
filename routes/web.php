854328724c358ec8cce7f275c6e7e3d7c6130503
KenTandrian <kenricktan11@gmail.com>
2023-03-22 10:43:48 +0700

    fix: web routing controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -16,54 +20,54 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/', 'FrontController@index');
-Route::get('/list', 'FrontController@list');
-Route::post('buy/{id}', 'FrontController@buy');
-Route::get('logout', 'FrontController@logout');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+ 
+Route::get('/', [FrontController::class, 'index']);
+Route::get('/list', [FrontController::class, 'list']);
+Route::post('buy/{id}', [FrontController::class, 'buy']);
+Route::get('logout', [FrontController::class, 'logout']);
 
 Route::get('/detail', function () {
     return view('detail');
 });
 
 Route::group(['prefix'=>'user', 'middleware' => ['auth', 'role:member']], function() {
-    Route::get('/mybook', 'FrontController@mybook');
+    Route::get('/mybook', [FrontController::class, 'mybook']);
 });
 
 Route::group(['prefix'=>'admin'], function() {
     Route::get('/',function() {
         return view('admin.index');
     });
-    Route::get('logout','UserController@logout');
+    Route::get('logout', [UserController::class, 'logout']);
     
     // Book management
     Route::group(['prefix'=>'book'], function() {
-        Route::get('/', 'BookController@index');
-        Route::get('create', 'BookController@create');
-        Route::post('/', 'BookController@store');
-        Route::get('{id}/edit', 'BookController@edit');
-        Route::post('{id}/update', 'BookController@update');
-        Route::delete('{id}', 'BookController@destroy');
+        Route::get('/', [BookController::class, 'index']);
+        Route::get('create', [BookController::class, 'create']);
+        Route::post('/', [BookController::class, 'store']);
+        Route::get('{id}/edit', [BookController::class, 'edit']);
+        Route::post('{id}/update', [BookController::class, 'update']);
+        Route::delete('{id}', [BookController::class, 'destroy']);
     });
 
     // User management
     Route::group(['prefix'=>'user'], function() {
-        Route::get('/', 'UserController@index');
-        Route::get('create', 'UserController@create');
-        Route::post('/', 'UserController@store');
-        Route::get('{id}/edit', 'UserController@edit');
-        Route::post('{id}/update', 'UserController@update');
-        Route::delete('{id}', 'UserController@destroy');
+        Route::get('/', [UserController::class, 'index']);
+        Route::get('create', [UserController::class, 'create']);
+        Route::post('/', [UserController::class, 'store']);
+        Route::get('{id}/edit', [UserController::class, 'edit']);
+        Route::post('{id}/update', [UserController::class, 'update']);
+        Route::delete('{id}', [UserController::class, 'destroy']);
     });
 
     // Transaction management
     Route::group(['prefix'=>'transaction'], function() {
-        Route::get('/', 'TransactionController@index');
-        Route::get('create', 'TransactionController@create');
-        Route::post('/', 'TransactionController@store');
-        Route::get('{id}/edit', 'TransactionController@edit');
-        Route::post('{id}/update', 'TransactionController@update');
-        Route::delete('{id}', 'TransactionController@destroy');
+        Route::get('/', [TransactionController::class, 'index']);
+        Route::get('create', [TransactionController::class, 'create']);
+        Route::post('/', [TransactionController::class, 'store']);
+        Route::get('{id}/edit', [TransactionController::class, 'edit']);
+        Route::post('{id}/update', [TransactionController::class, 'update']);
+        Route::delete('{id}', [TransactionController::class, 'destroy']);
     });
 });
