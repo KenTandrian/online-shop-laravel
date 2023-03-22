bd7e5c96de4aa6bb2baa53c8cbfa4f926e763e47
KenTandrian <kenricktan11@gmail.com>
2023-03-22 11:41:42 +0700

    fix: Graph container element not found

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -222,9 +222,9 @@
     <!-- AdminLTE App -->
     <script src="{{ URL::asset('adminlte/dist/js/adminlte.min.js')}}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ URL::asset('adminlte/dist/js/pages/dashboard.js')}}"></script>
+    {{-- <script src="{{ URL::asset('adminlte/dist/js/pages/dashboard.js')}}"></script> --}}
     <!-- AdminLTE for demo purposes -->
-    <script src="{{ URL::asset('adminlte/dist/js/demo.js')}}"></script>
+    {{-- <script src="{{ URL::asset('adminlte/dist/js/demo.js')}}"></script> --}}
     @yield('js')
 </body>
 </html>
